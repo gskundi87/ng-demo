3377ad424551c5061417f47422e9541930dac9ba
gskundi87 <gskundi87@gmail.com>
2023-01-23 22:54:34 -0800

    new

--- src/app/two/two.module.ts
+++ src/app/two/two.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestComponent } from './test/test.component';
 import { FooComponent } from './foo/foo.component';
+import { TestServiceService } from './test/test-service.service';
 
 
 
 @NgModule({
   declarations: [TestComponent, FooComponent],
   imports: [CommonModule],
-  exports: [TestComponent]
+  exports: [TestComponent],
+  providers: [TestServiceService]
 })
 export class TwoModule { }
